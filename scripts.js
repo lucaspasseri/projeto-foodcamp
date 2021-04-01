a50f25ce9af4ad1b4ebefd41b4fc132afc426a1c
Lucas Passeri <lucaspasseri@poli.ufrj.br>
2021-04-01 02:46:13 -0300

    Botão de finalizar pedido implementado, ficando verde quando um produto de cada tipo é selecionado

--- scripts.js
+++ scripts.js
@@ -5,7 +5,9 @@ function selecionar_principal(elemento){
 
     const produto_icone = elemento.querySelector("ion-icon");
 
-    if(!elemento.classList.contains("desativar-produto")){
+    const elemento_ativado = !elemento.classList.contains("desativar-produto");
+    
+    if(elemento_ativado){
         elemento.classList.toggle("desativar-produto");
         produto_icone.classList.toggle("desativar-icone");
     } else {
@@ -20,6 +22,7 @@ function selecionar_principal(elemento){
         elemento.classList.toggle("desativar-produto");
         produto_icone.classList.toggle("desativar-icone");
     }
+    finalizar_pedido();
 }
 function selecionar_bebida(elemento){
 
@@ -43,6 +46,7 @@ function selecionar_bebida(elemento){
         elemento.classList.toggle("desativar-produto");
         produto_icone.classList.toggle("desativar-icone");
     }
+    finalizar_pedido();
 }
 function selecionar_sobremesa(elemento){
 
@@ -66,4 +70,37 @@ function selecionar_sobremesa(elemento){
         elemento.classList.toggle("desativar-produto");
         produto_icone.classList.toggle("desativar-icone");
     }
+    finalizar_pedido();
 }
+function finalizar_pedido(){
+    const principais = document.querySelectorAll(".produto-principal");
+    const bebidas = document.querySelectorAll(".produto-bebida");
+    const sobremesas = document.querySelectorAll(".produto-sobremesa");
+
+    let produto_principal_selecionado = false;
+    let produto_bebida_selecionado = false;
+    let produto_sobremesa_selecionado = false;
+
+    for(let i = 0; i < principais.length; i++){
+        if (!principais[i].classList.contains("desativar-produto")){
+            produto_principal_selecionado = true;
+        }
+    }
+    for(let a = 0; a < bebidas.length; a++){
+        if (!bebidas[a].classList.contains("desativar-produto")){
+           produto_bebida_selecionado = true;
+        }
+    }
+    for(let b = 0; b < sobremesas.length; b++){
+        if (!sobremesas[b].classList.contains("desativar-produto")){
+            produto_sobremesa_selecionado = true;
+        }
+    }
+    const botao = document.querySelector("button");
+
+    if ((produto_principal_selecionado === true) && (produto_bebida_selecionado === true) && (produto_sobremesa_selecionado === true)){
+        botao.classList.remove("desativar-botao"); 
+    }else {
+        botao.classList.add("desativar-botao");
+    }
+} 
